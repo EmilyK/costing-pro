bbae60f0b857d15e8068943f08876fe79d78a508
Emily Karungi <ekar45@gmail.com>
2014-11-13 20:02:40 +0300

    added test for business model

---
+++ dashboard/test_models.py
@@ -0,0 +1,14 @@
+from models import BusinessProfile 
+from django.test import TestCase
+
+class BusinessProfileTest(TestCase):
+	def test_bussiness_profile_has_all_fields_listed(self):
+		business_profile = BusinessProfile()
+
+		fields =['business_name', 'category', 'city', 'division', 'village', 'parish', 'telephone_number']
+
+		for field in fields:
+			self.assertTrue(hasattr(business_profile,field))
+		#the len is 8 because django auto adds id for each model	
+		self.assertEqual(8, len(business_profile._meta.fields))
+
