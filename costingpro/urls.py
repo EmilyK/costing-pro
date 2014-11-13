8c5ba3d4be8e1323bd2e0b50c0bcd9c4f0738057
Emily Karungi <ekar45@gmail.com>
2014-11-13 15:42:33 +0300

    re-arranged the urls

--- costingpro/urls.py
+++ costingpro/urls.py
@@ -7,8 +7,7 @@ from registration.backends.simple.views import RegistrationView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'costingpro.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),    
+    # url(r'^$', 'costingpro.views.home', name='home'),   
     url(r'^accounts/', include('reg.urls')),
 	url(r'^$', 'dashboard.views.home', name='index'),
     url(r'^admin/', include(admin.site.urls)),
