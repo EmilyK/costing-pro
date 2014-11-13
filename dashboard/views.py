b3aa5d597d935782dda5a9e2c4306cfbbc2b08de
Emily Karungi <ekar45@gmail.com>
2014-11-13 15:45:50 +0300

    added the login view

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import RequestContext, render_to_response
 
 
 def home(request):
-	return render_to_response('dashboard/home.html', 
-		{}, 
-		RequestContext(request))
+	if request.user and request.user.is_authenticated():
+		return redirect("business_profile") # it has to exist..otherwise error
+	else:
+		return render_to_response('dashboard/home.html', 
+								  {}, 
+								  RequestContext(request))
