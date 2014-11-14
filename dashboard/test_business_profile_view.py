10cd2a2ba51f6c913684963f57695992f238b94f
Emily Karungi <ekar45@gmail.com>
2014-11-14 15:19:50 +0300

    added test for the business profile view to test template rendered

---
+++ dashboard/test_business_profile_view.py
@@ -0,0 +1,15 @@
+from views import business_profile
+from django.test import TestCase, Client
+
+#render template
+#template should have a form
+
+class BusinessProfileViewTest(TestCase):
+
+	def test_should_render_business_profile_template(self):
+		client = Client()
+		response = client.get('/business-profile/')
+		
+		self.assertEquals(200, response.status_code)
+		self.assertTemplateUsed(response, 'dashboard/business_profile.html')
+
