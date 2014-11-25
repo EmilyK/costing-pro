d5f277ff44ce1420ad673329abc106b38af67d2e
Emily Karungi <ekar45@gmail.com>
2014-11-25 21:24:16 +0300

    add costing-pro detail

--- costingpro/urls.py
+++ costingpro/urls.py
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
 	url(r'^accounts/signup/$', 'dashboard.views.signup', name='signup'),
 	url(r'^business_profile/$', 'dashboard.views.business_profile', name='business_profile'),
 	url(r'^business_profile/costing/$', 'dashboard.views.costing', name='costing'),
+	url(r'^costing/costing_detail/(?P<pk>\d+)/$', 'dashboard.views.costing_detail', name='costing_detail'),
 	url(r'^menu/$', 'dashboard.views.menu', name='menu'),
+
     url(r'^admin/', include(admin.site.urls)),
  )
