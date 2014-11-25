0422ac2cbc212cf52aadaa6faed18a9a3b341526
Emily Karungi <ekar45@gmail.com>
2014-11-25 19:33:44 +0300

    Add user foreignkey

--- dashboard/models.py
+++ dashboard/models.py
@@ -13,6 +13,7 @@ class BusinessProfile(models.Model):
 	category = models.CharField(max_length=15, choices=CATEGORIES)
 	telephone_number = models.CharField(max_length=10, blank=False)
 	
+	user = models.ForeignKey(User, null=True)
 	# phone_number = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
  #                                error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
 class UserSignUp(models.Model):
