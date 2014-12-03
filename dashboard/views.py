096078167701d907202eca67e31425f40b0243b0
Emily Karungi <ekar45@gmail.com>
2014-12-03 13:04:48 +0300

    refactor authentication

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,8 @@
-import user
 from django.shortcuts import RequestContext, render_to_response, redirect, HttpResponse, get_object_or_404
-from forms import BusinessProfileForm, UserSignUpForm, RawmaterialForm
+from forms import BusinessProfileForm, UserSignUpForm, RawmaterialForm, LoginForm
 from models import RawMaterial, BusinessProfile
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.core.context_processors import csrf, request
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
@@ -11,100 +10,96 @@ from django.db.models import Sum
 
 
 def home(request):
-    return render_to_response('dashboard/home.html',
-            {},
-                                  RequestContext(request))
+    if request.user.is_authenticated():
+        if BusinessProfile.objects.filter(user=request.user).exists():
+            return redirect('business_profiles')
+        return redirect('business_profile_new')
+    return render_to_response('dashboard/home.html', {},
+                              RequestContext(request))
 
 def  signup(request):
-    context = RequestContext(request)
-
     registered = False
+
+    form = UserSignUpForm()
 
     if request.method == 'POST':
         form = UserSignUpForm(data=request.POST)
 
         if form.is_valid():
             user = form.save()
-
             user.set_password(user.password)
             user.save()
-
-            profile = form.save(commit=False)
-            profile.user = user
-
-            profile.save()
             registered = True
         else:
             print form.errors
 
-    else:
-        form = UserSignUpForm()
-
     return render_to_response(
             'dashboard/signup.html',
             {'form': form, 'registered': registered},
-            context)
+            RequestContext(request))
 
-def login(request):
-    context = RequestContext(request)
+def _login(request):
+    form = LoginForm() 
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        form = LoginForm(request.POST)
 
-        user = authenticate(username=username, password=password)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
         
-        if user:
-            if user.is_active:
-                login(request, user)
-                return RediectView(url='/dashboard/business_profile.html')
+            user = authenticate(username=username, password=password)
+        
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('menu')
+                else:
+                    return HttpResponse("Your account is disabled.")
             else:
-                return HttpResponse("Your account is disabled.")
-        else:
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+                print "Invalid login details: {0}, {1}".format(username, password)
+                return HttpResponse("Invalid login details supplied.")
 
     else:
-        return render_to_response('registration/login.html', {}, context)
+        return render_to_response('registration/login.html', {'form': form},
+         RequestContext(request))
     
 
 def business_profile_list(request):
     if request.user.is_authenticated():
         business_profiles = BusinessProfile.objects.filter(user=request.user)
-        return render_to_response('dashboard/business_profile_list.html',
+        if business_profiles.exists():
+
+            return render_to_response('dashboard/business_profile_list.html',
             {'business_profiles': business_profiles}, RequestContext(request))
+        else:
+            messages.add_message(request, messages.INFO, "Please create a new business profile first")
+            return redirect('business_profile_new')
     else:
         return HttpResponse("You need to login first!")
 
 
 def business_profile(request):
-    c = {}
-    c.update(csrf(request))
+    form = BusinessProfileForm()
     if request.method == 'POST':
         form = BusinessProfileForm(request.POST)
-
         if form.is_valid():
-
             business_profile = form.save()
             if request.user.is_authenticated():                
                 # attribute user to business profile because of FK
                 business_profile.user = request.user
                 business_profile.save()
 
-            return menu(request)
+                return redirect('business_profiles')
 
         else:
             print form.errors
-            c['form'] = form
 
-    else:
-        form = BusinessProfileForm()
-        c['form'] = form
-
-    return render_to_response('dashboard/business_profile.html', c)
+    return render_to_response('dashboard/business_profile.html', 
+                              {'form': form, 'messages': messages.get_messages(request)}, 
+                              RequestContext(request))
 
 def menu(request):
-    return render_to_response('dashboard/menu.html',
-        {},
+    return render_to_response('dashboard/menu.html',{},
                               RequestContext(request))
 
 
@@ -147,6 +142,7 @@ def costing_raw_materials(request, pk):
     # TODO -> remove `all()` from below
     business_profile = get_object_or_404(BusinessProfile, pk=pk)
     total = 0.0
+
     if request.user.is_authenticated():
         raw_materials = RawMaterial.objects.filter(business_profile=business_profile)
         # raw_materials = RawMaterial.objects.all()
