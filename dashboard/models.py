8a91e7d66b4eeb6c0d0c0d55ff3e7827ae8df39d
Emily Karungi <ekar45@gmail.com>
2014-12-09 13:14:30 +0300

    add product and product material model

--- dashboard/models.py
+++ dashboard/models.py
@@ -18,9 +18,9 @@ class BusinessProfile(models.Model):
 		return self.business_name
 
 class RawMaterial(models.Model):
-	time_rate = models.CharField(max_length=50, choices=(('d', 'daily'), 
-		                         ('w', 'weekly'),
-		                         ('m', 'monthly')))
+	time_rate = models.CharField(max_length=50, choices=(('d', 'Daily'), 
+		                         ('w', 'Weekly'),
+		                         ('m', 'Monthly')))
 	name = models.CharField(max_length=50)
 	size = models.DecimalField(max_digits=10, default=0.00, decimal_places=3)
 	cost = models.DecimalField(max_digits=50, default=0.00, decimal_places=5)
@@ -30,3 +30,25 @@ class RawMaterial(models.Model):
 		return self.name
 
 
+class Product(models.Model):
+	name = models.CharField(max_length=50, verbose_name='Product name')
+	product_size = models.CharField(max_length = 8, choices=(('s', 'Small'), ('m', 'Medium'),
+									('l', 'Large')))
+	business_profile = models.ForeignKey(BusinessProfile, null=True, blank=True)
+
+	# State depicts whether product has been fully created with RawMaterials
+	created = models.BooleanField(default=False)
+	
+	def __unicode__(self):
+		return self.name
+
+
+class ProductRawMaterial(models.Model):
+	raw_material = models.ForeignKey(RawMaterial)
+	product = models.ForeignKey(Product)
+	amount = models.IntegerField(default=1)
+	business_profile = models.ForeignKey(BusinessProfile, null=True, blank=True)
+
+
+	def get_amount(self):
+		return "{0}".format(10)
