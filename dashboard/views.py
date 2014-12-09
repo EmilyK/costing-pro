d3097cfd76cc7a55a25a911511b879f0d8caafba
Emily Karungi <ekar45@gmail.com>
2014-12-09 13:17:58 +0300

    views modified

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import RequestContext, render_to_response, redirect, HttpResponse, get_object_or_404
-from forms import BusinessProfileForm, UserSignUpForm, RawmaterialForm, LoginForm
-from models import RawMaterial, BusinessProfile
+from forms import BusinessProfileForm, UserSignUpForm, RawmaterialForm, LoginForm, ProductForm, ProductRawMaterialForm
+from models import RawMaterial, BusinessProfile, Product, ProductRawMaterial
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import auth, messages
 from django.core.context_processors import csrf, request
@@ -68,7 +68,6 @@ def business_profile_list(request):
     if request.user.is_authenticated():
         business_profiles = BusinessProfile.objects.filter(user=request.user)
         if business_profiles.exists():
-
             return render_to_response('dashboard/business_profile_list.html',
             {'business_profiles': business_profiles}, RequestContext(request))
         else:
@@ -77,6 +76,10 @@ def business_profile_list(request):
     else:
         return HttpResponse("You need to login first!")
 
+def business_profile_detail(request, pk):
+    business_profile = get_object_or_404(BusinessProfile, pk=pk)
+    return render_to_response('dashboard/business_profile_detail.html',
+        {'business_profile': business_profile}, RequestContext(request))
 
 def business_profile(request):
     form = BusinessProfileForm()
@@ -103,6 +106,11 @@ def menu(request):
                               RequestContext(request))
 
 
+def all_products(request):
+    return render_to_response('dashboard/all_products.html',
+                              {'products': Product.objects.all()},
+                              RequestContext(request))
+
 def new_inventory(request, pk):
     form = RawmaterialForm()
 
@@ -121,14 +129,111 @@ def new_inventory(request, pk):
             return redirect('costing_raw_materials', pk=pk) #, id=raw_material.id)
 
         else:
-            # TODO -> hande errors here!
+            # TODO -> handle errors here!
             pass
 
     return render_to_response('dashboard/costing.html',
         {'form': form},
         RequestContext(request)
         )
-    
+
+def new_product(request, pk):
+    form = ProductForm()
+
+    business_profile = get_object_or_404(BusinessProfile, pk=pk)
+
+    if request.method == 'POST' and request.user.is_authenticated():
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+
+            product = form.save()
+            # attribute "business profile" to product
+            product.business_profile = business_profile
+            product.save()
+
+            messages.add_message(request, messages.INFO,
+                "You've created a product successfully. Please add raw materials used.")
+
+            return redirect('product_add_raw_material', pk=pk, id=product.id) #, id=product.id)
+
+        else:
+            # TODO -> handel errors here!
+            pass
+
+    return render_to_response('dashboard/new_product.html',
+        {'form': form},
+        RequestContext(request)
+        )
+
+
+def product(request, pk, id):
+    _product = get_object_or_404(Product, id=id)
+    business_profile = get_object_or_404(BusinessProfile, pk=pk)
+    product_rawmaterials = _product.productrawmaterial_set.all()
+    return render_to_response('dashboard/product.html', {'product': _product, 
+        'product_rawmaterials': product_rawmaterials, 'business_profile': business_profile}, 
+                                RequestContext(request))
+
+def product_add_raw_material(request, pk, id):
+    product = get_object_or_404(Product, id=id)
+    business_profile = get_object_or_404(BusinessProfile, pk=pk)
+
+    form = ProductRawMaterialForm(business_profile)
+
+    user = request.user
+
+    if business_profile.user == user and user.is_authenticated():
+        if request.method == "POST":
+            form = ProductRawMaterialForm(business_profile, request.POST)
+            if form.is_valid():
+                
+                raw_material = get_object_or_404(RawMaterial, id=form.cleaned_data['raw_material']) #TODO
+                amount = int(form.cleaned_data['amount'])
+                
+                if amount > raw_material.size or amount <= 0:
+                    messages.add_message(request, messages.ERROR,
+                        "The amount cannot be greater than your inventory. Current size is {0}".format(raw_material.size))
+                else:
+                    product_raw_material = ProductRawMaterial.objects.create(
+                        raw_material = raw_material,
+                        product = product,
+                        business_profile = business_profile,
+                        amount = amount)
+                    product_raw_material.save()
+                    # reduce inventory
+                    raw_material.size -= amount
+                    raw_material.save()
+                    if product.created:
+                        messages.add_message(request, messages.INFO, 
+                            "You've updated your raw materials")
+                    else:
+                        product.created = True
+                        product.save()
+                        messages.add_message(request, messages.INFO,
+                            "You've successfully created your product")
+                    return redirect("product", pk=business_profile.pk, id=product.id )
+
+            else:
+                messages.add_message(request, messages.INFO, 
+                    "The form input is invalid!")
+        else:
+            pass
+    else:
+        messages.add_message(request, messages.INFO, 
+                             "You are forbidden from adding raw materials")
+    return render_to_response('dashboard/add_raw_material_to_product.html',
+                              {'form': form, 'messages': messages.get_messages(request)}, RequestContext(request))
+
+
+def products(request, pk):
+    business_profile = get_object_or_404(BusinessProfile, pk=pk)
+    products = ProductRawMaterial.objects.filter(business_profile=business_profile)
+    return render_to_response('dashboard/business_profile_product_list.html',
+                              {'products': products},
+                              RequestContext(request))
+
+
 def costing_detail(request, pk, id):
     # TODO -> change name of function to `raw_material`
     business_profile = get_object_or_404(BusinessProfile, pk=pk)
