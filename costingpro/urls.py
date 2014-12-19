ff0fc178c4c8b23dc67280adf806cc0f72fe328b
Emily Karungi <ekar45@gmail.com>
2014-12-19 15:45:54 +0300

    making heroku fixes

--- costingpro/urls.py
+++ costingpro/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from dashboard import views
-
+from costingpro import settings
 
 
 
@@ -48,3 +47,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
  )
+
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
